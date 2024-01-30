48af1cf772dcfc4c783017fecdb6c59bc883568f
Giannina Callegari <111541072+gcallegari19@users.noreply.github.com>
2024-01-30 16:50:20 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './styles/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './components/NavBar';
-import ItemListContainer from './components/ItemListContainer';
+import NavBar from './components/Navbar/NavBar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 
 const App = () => {
